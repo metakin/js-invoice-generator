a4d805905b2c3cba3ad70b11fef0c76297ec0341
metakin <metakin30@gmail.com>
2022-12-27 20:22:52 +0300

    Update: Builded File Name

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'motion.js',
+		filename: 'motion.min.js',
 		library: 'motion',
 		libraryTarget: 'umd',
 		libraryExport: 'default'
